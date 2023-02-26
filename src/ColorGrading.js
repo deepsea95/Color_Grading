048764c17a6269db87fa518d0d4980a8a6e8e506
Angelo De Rosa <angelo.derosa95@gmail.com>
2023-02-26 16:51:48 +0100

    refactor app

--- src/ColorGrading.js
+++ src/ColorGrading.js
@@ -1,82 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import Values from 'values.js';
-import { v4 as uuidv4 } from 'uuid';
-import SingleColor from './SingleColor';
+import React, { useState, useEffect } from "react";
+import Values from "values.js";
+import { v4 as uuidv4 } from "uuid";
+import SingleColor from "./SingleColor";
 
 function ColorGrading() {
-	const [ selectedColor, setSelectedColor ] = useState([]);
-	const [ isError, setIsError ] = useState(false);
-	const [ colorInput, setColorInput ] = useState({
-		color: '',
-		qty: 10
-	});
+  // Dichiarazione degli stati utilizzati nel componente
+  const [selectedColor, setSelectedColor] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [colorInput, setColorInput] = useState({
+    color: "",
+    qty: 10,
+  });
 
-	const color = new Values('rgb(0, 153, 255)');
-	color.all(10);
+  // Creazione di una nuova istanza della libreria "Values" con un colore iniziale (blu)
+  const color = new Values("rgb(0, 153, 255)");
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (colorInput.color && colorInput.qty) {
-			const { color, qty } = colorInput;
-			try {
-				setSelectedColor(new Values(color).all(Math.round(100 / parseInt(qty, 10)) * 2));
-				setColorInput({
-					color: '',
-					qty: 10
-				});
-			} catch (error) {
-				setIsError(true);
-			}
-		}
-	};
+  // Creazione di un array di 10 colori generati dalla libreria "Values"
+  color.all(10);
 
-	const handleChange = (e) => {
-		setIsError(false);
-		const { name, value } = e.target;
-		setColorInput({ ...colorInput, [name]: value });
-	};
+  // Funzione che viene chiamata quando viene inviato il form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (colorInput.color && colorInput.qty) {
+      // Estrazione delle proprietà "color" e "qty" dall'oggetto "colorInput"
+      const { color, qty } = colorInput;
 
-	useEffect(() => {
-		setColorInput({ qty: 10, color: '#2b2b2b' });
-		setSelectedColor(new Values('#2b2b2b').all(Math.round(100 / 10)) * 2);
-	}, []);
+      try {
+        // Generazione di un array di colori utilizzando la libreria "Values"
+        setSelectedColor(
+          new Values(color).all(Math.round(100 / parseInt(qty, 10)) * 2)
+        );
 
-	return (
-		<React.Fragment>
-			<form className="form" onSubmit={handleSubmit}>
-				<div className="input-group">
-					<input
-						type="text"
-						name="color"
-						id="color"
-						value={colorInput.color}
-						maxLength={7}
-						onChange={handleChange}
-					/>
-					<input
-						type="number"
-						name="qty"
-						id="qty"
-						value={colorInput.qty}
-						max={100}
-						min={5}
-						step={5}
-						onChange={handleChange}
-					/>
-				</div>
-				<button className="btn btn-selector" type="submit">
-					Create
-				</button>
-			</form>
-			<section className="color-section">
-				{isError ? (
-					<h4>Nessun Colore Trovato !!</h4>
-				) : (
-					selectedColor.length > 0 && selectedColor.map((el) => <SingleColor key={uuidv4()} {...el} />)
-				)}
-			</section>
-		</React.Fragment>
-	);
+        // Reimpostazione del valore dell'input di colore e della quantità di colori
+        setColorInput({
+          color: "",
+          qty: 10,
+        });
+      } catch (error) {
+        // In caso di errore nella generazione dei colori, impostiamo lo stato "isError" a true
+        setIsError(true);
+      }
+    }
+  };
+
+  // Funzione che viene chiamata quando viene modificato il valore di un input
+  const handleChange = (e) => {
+    // Reimpostazione dello stato "isError" a false
+    setIsError(false);
+    // Estrazione del nome e del valore dell'input che è stato modificato
+    const { name, value } = e.target;
+    // Aggiornamento dello stato "colorInput" con il nuovo valore dell'input
+    setColorInput({ ...colorInput, [name]: value });
+  };
+
+  // Effetto che viene eseguito quando il componente viene caricato per la prima volta
+  useEffect(() => {
+    // Inizializzazione dei valori dell'input di colore e della quantità di colori
+    setColorInput({ qty: 10, color: "#3892c2" });
+    // Generazione di un array di 20 colori utilizzando la libreria "Values"
+    setSelectedColor(new Values("#3892c2").all(Math.round(100 / 10)) * 2);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            name="color"
+            id="color"
+            value={colorInput.color}
+            maxLength={7}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="qty"
+            id="qty"
+            value={colorInput.qty}
+            max={100}
+            min={5}
+            step={5}
+            onChange={handleChange}
+          />
+          <button className="btn btn-selector" type="submit">
+            Create
+          </button>
+        </div>
+      </form>
+      <section className="color-section">
+        {isError ? (
+          <h4>Nessun Colore Trovato !!</h4>
+        ) : (
+          selectedColor.length > 0 &&
+          selectedColor.map((el) => <SingleColor key={uuidv4()} {...el} />)
+        )}
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default ColorGrading;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,36 +1,47 @@
-import rgbToHex from './Utils/helpers';
-import React, { useState, useEffect } from 'react';
+import rgbToHex from "./Utils/helpers";
+import React, { useState, useEffect, useMemo } from "react";
 
 function SingleColor({ rgb, type, weight }) {
-	const [ message, setMessage ] = useState(false);
+  // Stato per indicare se il messaggio "Colore copiato!" deve essere visualizzato
+  const [message, setMessage] = useState("");
 
-	const copiaColore = () => {
-		navigator.clipboard.writeText(rgbToHex(...rgb)).then(() => setMessage(true)).catch((err) => console.log(err));
-	};
+  // Funzione per copiare il valore esadecimale del colore nella clipboard
+  const copiaColore = () => {
+    navigator.clipboard
+      .writeText(rgbToHex(...rgb))
+      .then(() => setMessage("Colore copiato!"))
+      .catch((err) => console.log(err));
+  };
 
-	useEffect(
-		() => {
-			const timer = setTimeout(() => {
-				setMessage(false);
-			}, 2000);
+  // Effetto per rimuovere il messaggio dopo 2 secondi
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage("");
+    }, 2000);
 
-			return () => clearTimeout(timer);
-		},
-		[ message ]
-	);
+    // Pulizia dell'effetto
+    return () => clearTimeout(timer);
+  }, [message]);
 
-	return (
-		<div style={{}}>
-			<article
-				onClick={copiaColore}
-				className={`single-color ${type}`}
-				style={{ backgroundColor: rgbToHex(...rgb), width: '300px', height: '300px',}}
-			>
-				<h5>{rgbToHex(...rgb)}</h5>
-				{message && <p>Colore copiato !</p>}
-			</article>
-		</div>
-	);
+  // Calcola il valore esadecimale del colore solo quando cambia `rgb`
+  const hex = useMemo(() => rgbToHex(...rgb), [rgb]);
+
+  return (
+    <div style={{}}>
+      <article
+        onClick={copiaColore}
+        className={`single-color ${type}`}
+        style={{
+          backgroundColor: rgbToHex(...rgb),
+          width: "300px",
+          height: "300px",
+        }}
+      >
+        <h5>{hex}</h5>
+        {message && <p>Colore copiato !</p>}
+      </article>
+    </div>
+  );
 }
 
 export default SingleColor;
